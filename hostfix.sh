52dac2dab7447c8cd382627f90d993714823a434
Tiago Mota <tiago.mota.900@gmail.com>
2023-01-09 19:47:00 +0000

    feat: add proxied property default true

--- hostfix.sh
+++ hostfix.sh
@@ -21,8 +21,14 @@ if [ -z "$API_TOKEN" ]; then
     exit 1
 fi
 
+
+if [ -z "$PROXIED" ]; then
+    PROXIED="true"
+fi
+
+
 SELF_PUBLIC_IP=$(curl -s 'https://api.ipify.org?format=json' | grep -o '[0-9\.]\+');
 curl -X PUT "https://api.cloudflare.com/client/v4/zones/${ZONE_ID}/dns_records/${RECORD_ID}" \
 -H "Authorization: Bearer ${API_TOKEN}" \
 -H "Content-Type: application/json" \
---data "{\"type\":\"A\",\"name\":\"${HOST_NAME}\",\"content\":\"${SELF_PUBLIC_IP}\",\"ttl\":3600,\"proxied\":true}";
+--data "{\"type\":\"A\",\"name\":\"${HOST_NAME}\",\"content\":\"${SELF_PUBLIC_IP}\",\"ttl\":3600,\"proxied\":${PROXIED}}";
